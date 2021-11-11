1c23065f8d5756e72895b1de05dcfb25ac18e16d
Patryk Mazur <pat.mazur5@gmail.com>
2021-11-11 16:43:54 +0100

    siur

--- BetterOdrabiamyDownloader.py
+++ BetterOdrabiamyDownloader.py
@@ -22,30 +22,26 @@ token = json.loads(rpost).get('data').get('token')
 def upload(data):
     table=['Y','y','T','t','']
     if any(map(lambda x: x==share,table)) == True:
-        try:
-            clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            clientSocket.connect(("192.168.0.250",8100))
-            clientSocket.send(data.encode('utf-8'))
-        except:
-            pass
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        clientSocket.sendto(data.encode('utf-8'), ("192.168.0.100", 8100))
 
 def download_page(token, page, bookid):
     rget = requests.get(url=f'https://odrabiamy.pl/api/v2/exercises/page/premium/{page}/{bookid}', headers={'user-agent':'new_user_agent-huawei-142','Authorization': f'Bearer {token}'}).content.decode('utf-8')
     lists=json.loads(rget).get('data')
 
     name=lists[0].get('book').get('name').replace('/','')
-    upload(data=json.dumps(lists))
+    upload(data=rget)
     if not os.path.exists(f'{path}/{name}'):
         os.makedirs(f'{path}/{name}')
         os.makedirs(f'{path}/{name}/{page}')
 
-    for list in lists:
+    for exercise in lists:
         if not os.path.exists(f'{path}/{name}/{page}'):
             os.makedirs(f'{path}/{name}/{page}')
-        number=list.get('number')
+        number=exercise.get('number')
         file=open(f'{path}/{name}/{page}/index.html', 'a+', encoding='utf-8')
-        file.write(f'<head><meta charset="UTF-8"></head>\n<a style="color:red; font-size:25px;">Zadanie {number}</a>\n'+list.get('solution'))
-        file.close
+        file.write(f'<head><meta charset="UTF-8"></head>\n<a style="color:red; font-size:25px;">Zadanie {number}</a><br>\n'+exercise.get('solution')+'<br>')
+        file.close()
         
 def download_book(bookid):
     rget = requests.get(url=f'https://odrabiamy.pl/api/v1.3/ksiazki/{bookid}').content.decode('utf-8')
@@ -54,8 +50,9 @@ def download_book(bookid):
     for page in pages:
         if not os.path.exists(f'{path}/{name}/{page}'):
             seconds=random.randint(2,8)
-            download_page(token=token, page=page, bookid=bookid)
+            download_page(token, page, bookid)
             print(f'Pobrano stronę {page}\nNastępna strona zostanie pobrana za {seconds} sekund')
             time.sleep(seconds)
+    print('Pobrano książkę!')
 
 download_book(bookid=bookid)
